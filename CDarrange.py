335dfdcceedf8b5692a33c2be408fd59a6fd5c75
yk <yk@git.dhdo.net>
2013-02-23 01:23:31 +0900

    外部プログラムを用いた圧縮
    7zによるzip圧縮を実装

--- CDarrange.py
+++ CDarrange.py
@@ -1,17 +1,29 @@
 import sys
 import os
-import glob 
+import glob
+import re
+import zipfile
+import subprocess
 if len(sys.argv) > 1:
     if os.path.isfile(sys.argv[1]):
         workplace = os.path.dirname(sys.argv[1])
     else:
         workplace = sys.argv[1]
 else:
-    workplace = '.'
+    workplace = sys.path[0]
 os.chdir(workplace)
 
 files = glob.glob('*.log')
 
 for file in files:
-    print(file)
+    f = open(file)
+    lines = f.read().split('\n')
+    f.close()
+    CDfilename = re.sub(" / ", " - ", lines[1], 1)
+    print(CDfilename)
+    os.rename(file, CDfilename + ".log")
+    zipfiles = glob.glob(CDfilename + ".*")
+    command = ["7z", "a", "-tzip", CDfilename + ".zip"] + zipfiles
+    print(' '.join(command))
+    subprocess.check_call(command, shell=True)
 input()
