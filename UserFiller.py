07ff8b7134b4cac842c774b7cc3f9bd47211f36a
yk <yk@git.dhdo.net>
2013-03-02 12:49:58 +0900

    新規スクリプト
    W:\ (USER)をダミーファイルで埋めるためのスクリプト

---
+++ UserFiller.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import glob
+import subprocess
+if len(sys.argv) > 1:
+    if sys.argv[1].isdigit():
+        dummynum = int(sys.argv[1])
+    else:
+        print("引数に数字を指定してください")
+        input()
+        exit
+else:
+    print("引数がありません")
+    input()
+    exit
+os.chdir(sys.path[0])
+sourcefile = "DUMMY_FILES.zip"
+
+folders = glob.glob("W:/FILES/*")
+pivot = len(folders)
+for i in range(0, dummynum):
+    command = ["7z", "e", sourcefile, "-oW:/FILES/FILES" + str(pivot).zfill(3)]
+    subprocess.check_call(command, shell=True)
+    pivot += 1
+input()
