56a4107447f3f35748dbb39ee4634677f90ad60e
yk <yk@git.dhdo.net>
2013-03-06 11:27:14 +0900

    update
    処理時間の表示

--- UserFiller.py
+++ UserFiller.py
@@ -2,6 +2,8 @@ import os
 import sys
 import glob
 import subprocess
+import datetime
+from context_timer import Timer
 if len(sys.argv) > 1:
     if sys.argv[1].isdigit():
         dummynum = int(sys.argv[1])
@@ -16,11 +18,13 @@ else:
 os.chdir(sys.path[0])
 sourcefile = "dat/DUMMY_FILES.zip"
 
-folders = glob.glob("W:/FILES/*")
-pivot = len(folders)
-for i in range(0, dummynum):
-    command = ["7z", "e", sourcefile, "-oW:/FILES/FILES" + str(pivot).zfill(3)]
-
-    subprocess.check_call(command, shell=True)
-    pivot += 1
+with Timer():
+    folders = glob.glob("W:/FILES/*")
+    pivot = len(folders)
+    for i in range(0, dummynum):
+        command = ["7z", "e", sourcefile, "-oW:/FILES/FILES" + str(pivot).zfill(3)]
+        print("【起動】7zip.exe")
+        subprocess.check_call(command, shell=True)
+        print("【終了】7zip.exe")
+        pivot += 1
 input()
