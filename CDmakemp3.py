9134e888856cb26fdf543bae0290917d3b6f6948
yk <git@dhdo.net>
2013-05-11 17:59:52 +0900

    bugfix
    ファイル名に&が入っていると変換できない問題を修正

--- CDmakemp3.py
+++ CDmakemp3.py
@@ -19,7 +19,7 @@ wavfiles = glob.glob('*.wav')   #wavファイルを取得
 for wavfile in wavfiles:
     CDname = re.sub(".wav", '', wavfile)
     print("分割->mp3変換:" + CDname)
-    mp3Path = "D:/Users/yk2/Music/" + re.sub("[\\\"<>:/|?* ]", '', CDname)
+    mp3Path = "D:/Users/yk2/Music/" + re.sub("[\\\"<>:/|?* &]", '', CDname)  #ファイル名に使用できない文字のほか、&も使用できない様子
     if not os.path.isdir(mp3Path):
         os.mkdir(mp3Path)   #shntoolは特殊な文字を受け付けないようなので事前に変換している
     command = 'shntool split "' + CDname + '.wav" -f "' + CDname + '.cue" -o "cust ext=mp3 "D:/Programs/lame-3.98.4/x64/lame.exe" --preset insane - -o "' + mp3Path + '/%f"" -t "%n %t" '
